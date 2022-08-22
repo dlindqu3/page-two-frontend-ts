5769458f84061e8ebf8cdc595c2ef4d7a1f0a208
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-08-21 20:49:58 -0400

    added skeleton routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SavedBooks from './pages/SavedBooks';
+import About from './pages/About';
 
 function App() {
   return (
     <div className="App">
-      <p>hello</p>
+      <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        >
+        </Route>
+        <Route
+          path="/saved-books"
+          element={<SavedBooks />}
+        >
+        </Route>
+        <Route
+          path="/about"
+          element={<About />}
+        >
+        </Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/SavedBooks.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SavedBooks() {
+  return (
+    <div>
+      <p>SavedBooks page here</p>
+    </div>
+  )
+}
+
+export default SavedBooks
